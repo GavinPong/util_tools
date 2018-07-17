91b6376ba616956abba44c99da8ccbf00ed797c9
medianext <phight@163.com>
2018-07-17 11:14:16 +0800

    修复获取json文件时，一直异常的问题

--- code/filefunc/filefunc.c
+++ code/filefunc/filefunc.c
@@ -20,14 +20,14 @@ int32_t FILE_FUNC_Get_Json_Form_File(const char *path_name, cJSON **out_json){
 	{
 		return FILE_FUNC_ERR_OPENFILE;
 	}
-	fseek(fp, SEEK_END, 0);
+	fseek(fp, 0, SEEK_END);
 	file_size = ftell(fp);
 	if (!file_size)
 	{
 		fclose(fp);
 		return FILE_FUNC_ERR_EMPTY; 
 	}
-	fseek(fp, SEEK_SET, 0);
+	fseek(fp, 0, SEEK_SET);
 	file_buf = (char *)malloc(file_size);
 	if (!file_buf)
 	{
@@ -35,13 +35,14 @@ int32_t FILE_FUNC_Get_Json_Form_File(const char *path_name, cJSON **out_json){
 		return FILE_FUNC_ERR_OVERLOW; 
 	}
 	fread(file_buf, file_size, 1, fp);
-	free(file_buf);
 	*out_json = cJSON_Parse(file_buf);
 	if (!(*out_json))
 	{
+		free(file_buf);
 		fclose(fp);
 		return FILE_FUNC_ERR_PARSE_JSON;
 	}
+	free(file_buf);
 	fclose(fp);
 	return FILE_FUNC_OK;
 }
