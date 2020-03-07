faa5f6937aefc801d1a1f0e373a65fad45b8aab9
gavinpong <1910915035@qq.com>
2020-03-07 15:51:13 +0800

    1.优化error逻辑：通用错误值不会每个模块都malloc一块空间单独存储，而是所有模块共用 2.新增使用流程说明

--- code/error/gavin_error.c
+++ code/error/gavin_error.c
@@ -1,20 +1,88 @@
 #include "gavin_error.h"
+#include <stdint.h>
+
+#define get_comm_err_info(err_value) do \
+{\
+	int32_t err_id = GAVIN_GET_ERR_ID_BY_ERR((err_value));\
+	if (err_id <= GAVIN_ERR_FRAME_RATE)\
+	{\
+		strncpy(out_buf, g_common_err_info_set[err_id].m_info_ptr, buf_size - 1);\
+		out_buf[buf_size - 1] = '\0';\
+		return GAVIN_SUCCESS;\
+	}\
+} while (0)
 
 typedef struct _err_info_s{
-	char m_name[32];
-	get_err_info_cb_t m_get_err_info_cb;
+	int8_t m_name[32];
+	get_custom_err_info_cb_t m_get_err_info_cb;
 }err_info_t;
 
 typedef struct _err_info_list_node_s{
 	struct list_head m_list;
 	int32_t m_err_id;
-	char *m_info_ptr;
+	int8_t *m_info_ptr;
 }err_info_list_node_t;
 
 static err_info_t err_info_set[GAVIN_MODE_ID_MAX] = {};
+static err_info_list_node_t g_common_err_info_set[GAVIN_ERR_FRAME_RATE - 1] = {};
 
 int32_t gavin_err_init()
 {
+	//load err ionfo for commom error
+	int32_t i = 0;
+	int32_t total_cnt = sizeof(g_common_err_info_set);//Currently defined maximum error value
+	for (; i < total_cnt; i++)
+	{
+		g_common_err_info_set[i].m_err_id = i + 1;
+		if (GAVIN_ERR_INVALID_DEVID == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_INVALID_DEVID_STR;
+		else if(GAVIN_ERR_INVALID_CHNID == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_INVALID_CHNID_STR;
+		else if (GAVIN_ERR_ILLEGAL_PARAM == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_ILLEGAL_PARAM_STR;
+		else if (GAVIN_ERR_EXIST == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_EXIST_STR;
+		else if (GAVIN_ERR_UNEXIST == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_UNEXIST_STR;
+		else if (GAVIN_ERR_NULL_PTR == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_NULL_PTR_STR;
+		else if (GAVIN_ERR_NOT_CONFIG == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_NOT_CONFIG_STR;
+		else if (GAVIN_ERR_NOT_SUPPORT == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_NOT_SUPPORT_STR;
+		else if (GAVIN_ERR_NOT_PERM == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_NOT_PERM_STR;
+		else if (GAVIN_ERR_NOMEM == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_NOT_NOMEM_STR;
+		else if (GAVIN_ERR_NOBUF == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_NOT_NOBUF_STR;
+		else if (GAVIN_ERR_BUF_EMPTY == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_BUF_EMPTY_STR;
+		else if (GAVIN_ERR_BUF_FULL == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_BUF_FULLE_STR;
+		else if (GAVIN_ERR_SYS_NOTREADY == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_SYS_NOTREADY_STR;
+		else if (GAVIN_ERR_BADADDR == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_BADADDR_STR;
+		else if (GAVIN_ERR_BUSY == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_BUSY_STR;
+		else if (GAVIN_ERR_MODULE_ID == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_MODULE_ID_STR;
+		else if (GAVIN_ERR_PARSE_JSON == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_PARSE_JSON_STR;
+		else if (GAVIN_ERR_JSON_OBJ_EMPTY == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_JSON_OBJ_EMPTY_STR;
+		else if (GAVIN_ERR_JSON_SET_EMPTY == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_JSON_SET_EMPTY_STR;
+		else if (GAVIN_ERR_MATCH == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_MATCH_STR;
+		else if (GAVIN_ERR_INITED == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_INITED_STR;
+		else if (GAVIN_ERR_MUXTYPE == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_INVALID_MUXTYPE_STR;
+		else if (GAVIN_ERR_FRAME_RATE == i + 1)
+			g_common_err_info_set[i].m_info_ptr = (int8_t *)ERR_FRAME_RATE_STR;
+	}
 
 	return GAVIN_SUCCESS;
 }
@@ -25,7 +93,7 @@ int32_t gavin_err_uninit()
 	return GAVIN_SUCCESS;
 }
 
-int32_t gavin_err_register_output_func(int32_t module_id, const char*module_name, get_err_info_cb_t get_err_info_cb)
+int32_t gavin_err_register_output_func(int32_t module_id, const int8_t *module_name, get_custom_err_info_cb_t get_err_info_cb)
 {
 	if (module_id >= GAVIN_MODE_ID_MAX || module_id <= GAVIN_MODE_ID_MIN \
 		|| !module_name || !strlen(module_name)\
@@ -56,14 +124,14 @@ int32_t gavin_err_unregister_output_func(int32_t module_id)
 	return GAVIN_SUCCESS;
 }
 
-int32_t gavin_err_get_err_reason(int32_t err_value, char *out_buf, int32_t buf_size)
+int32_t gavin_err_get_err_reason(int32_t err_value, int8_t *out_buf, int32_t buf_size)
 {
 	if (!out_buf || buf_size <= 0)
 	{
 		return GAVIN_ERR_ILLEGAL_PARAM;
 	}
+	get_comm_err_info(err_value);
 	int32_t module_id = GAVIN_GET_MODULE_ID_BY_ERR(err_value);
-
 	if (module_id >= GAVIN_MODE_ID_MAX || module_id <= GAVIN_MODE_ID_MIN)
 	{
 		return GAVIN_ERR_UNEXIST;
@@ -95,13 +163,13 @@ static int32_t gavin_err_is_exist(struct list_head *list_head, int32_t err_id)
 	return GAVIN_FAILURE;
 }
 
-int32_t gavin_err_info_list_add_node(struct list_head *list_head, int32_t err_id, const char *err_info)
+int32_t gavin_err_info_list_add_node(struct list_head *list_head, int32_t err_id, const int8_t *err_info)
 {
 	if (!list_head || !err_info)
 	{
 		return GAVIN_ERR_NULL_PTR;
 	}
-	if (err_id <= 0)
+	if (err_id <= 0 || err_id <= GAVIN_ERR_BUTT)
 	{
 		return GAVIN_ERR_ILLEGAL_PARAM;
 	}
@@ -117,6 +185,7 @@ int32_t gavin_err_info_list_add_node(struct list_head *list_head, int32_t err_id
 		{
 			return GAVIN_ERR_NOMEM;
 		}
+		memcpy(err_info_list_node->m_info_ptr, err_info, strlen(err_info));
 		err_info_list_node->m_err_id = err_id;
 		list_add(&err_info_list_node->m_list, list_head);
 	}
@@ -126,12 +195,17 @@ int32_t gavin_err_info_list_add_node(struct list_head *list_head, int32_t err_id
 
 int32_t gavin_err_info_list_del_node(struct list_head *list_head, int32_t err_id)
 {
-	if (!list_head || err_id <=0)
+	struct list_head *head = list_head;
+	err_info_list_node_t *cur, *next;
+
+	if (!list_head || err_id <= 0)
 	{
 		return GAVIN_ERR_NULL_PTR;
 	}
-	struct list_head *head = list_head;
-	err_info_list_node_t *cur, *next;
+	if (err_id <= 0 || err_id <= GAVIN_ERR_BUTT)
+	{
+		return GAVIN_ERR_ILLEGAL_PARAM;
+	}
 	list_for_each_entry_safe(err_info_list_node_t, cur, next, head, m_list)
 	{
 		if (cur->m_err_id == err_id)
@@ -149,7 +223,7 @@ int32_t gavin_err_info_list_del_node(struct list_head *list_head, int32_t err_id
 	return GAVIN_ERR_UNEXIST;
 }
 
-int32_t gavin_err_info_list_find_err_info(struct list_head *list_head, int32_t err_id, char *out_buf, int32_t buf_size)
+int32_t gavin_err_info_list_find_err_info(struct list_head *list_head, int32_t err_id, int8_t *out_buf, int32_t buf_size)
 {
 	if (!list_head || !out_buf || buf_size <=0)
 	{
@@ -158,6 +232,9 @@ int32_t gavin_err_info_list_find_err_info(struct list_head *list_head, int32_t e
 	int32_t flag = 0;
 	struct list_head *head = list_head;
 	err_info_list_node_t *cur, *next;
+
+	get_comm_err_info(err_id);//err_id 可以替代err_value，因为只是提取错误值
+
 	list_for_each_entry_safe(err_info_list_node_t, cur, next, head, m_list)
 	{
 		if (cur->m_err_id == err_id)
--- code/error/gavin_error.h
+++ code/error/gavin_error.h
@@ -16,11 +16,32 @@ History       :
   Modification: Created file
 
 ******************************************************************************/
+/*
+关键字说明：
+1、模块ID：模块分配人员在分配模块时，在err.h中的gavin_mode_id_e指定的模块ID
+2、错误ID：在err.h中的gavin_err_level_id_e值以及模块开发人员在自己的模块代码中新增的错误ID；
+3、错误值：GAVIN_DEF_ERR
+
+err接口使用流程：
+1、没有不同于通用错误的其他错误信息的场景：
+| --根据给定的模块ID 为模块定义错误信息（参考err_dummy.h）；
+| --调用gavin_err_get_err_reason根据错误值获取具体的错误信息；
+
+
+2、有不同于通用错误的其他错误信息的场景：
+a、定义自己模块特有错误ID，起始值必须是大于gavin_err_code_id_e中的最大值（参考err_dummy.h）；
+b、根据给定的模块ID和错误ID为模块定义错误值信息（参考err_dummy.h）；
+c、在模块上下文管理字段中添加list_head用于关联自己模块特有得出错误
+d、在模块初始时调用gavin_err_info_list_add_node将自己模块特有错误加入到c中的list_head链表中；
+e、定义定义get_custom_err_info_cb_t回调函数，里面完成根据错误ID在c的list_head中获取模块自定义错误信息的逻辑；
+f、调用gavin_err_register_output_func将get_custom_err_info_cb_t回调函数和模块ID注册到err.h模块中
+g、调用gavin_err_get_err_reason根据错误值获取具体的错误信息；
+*/
+
 
 #ifndef __GAVIN_ERROR_H__
 #define __GAVIN_ERROR_H__
 
-#include "cross_platform.h"
 #include "list.h"
 
 /******************************************************************************
@@ -73,6 +94,7 @@ typedef enum _gavin_err_level_id_e
 
 /* NOTE! the following defined all common error code,
 ** all module must reserved 0~63 for their common error code
+** member must be assigned for compatibility
 */
 typedef enum _gavin_err_code_id_e
 {
@@ -107,6 +129,20 @@ typedef enum _gavin_err_code_id_e
 
     GAVIN_ERR_BUSY          = 18,/* resource is busy, 
                               ** eg. destroy a venc chn without unregister it */
+	GAVIN_ERR_MODULE_ID		= 19,/* invalid device id */
+
+	GAVIN_ERR_PARSE_JSON	= 20,/* parse json failed */
+						  
+	GAVIN_ERR_JSON_OBJ_EMPTY = 21,/* json data is empty */
+	GAVIN_ERR_JSON_SET_EMPTY = 22,/* json set is empty */
+
+	GAVIN_ERR_MATCH			= 23,/* match failure */
+	GAVIN_ERR_INITED		= 24,/* Already initialized */
+					  
+	GAVIN_ERR_MUXTYPE		= 25,/* invalid rec muxer type  */
+						  
+	GAVIN_ERR_FRAME_RATE	= 26,/* frame rate anomaly*/
+						  
 
     GAVIN_ERR_BUTT          = 63,/* maxium code, private error code of all modules
                               ** must be greater than it                      */
@@ -119,40 +155,40 @@ typedef enum _gavin_err_code_id_e
 */
 
 //err info table
-#define INVALID_DEVID		"invalid device id"
-#define INVALID_CHNID		"invalid channel id"
-#define ILLEGAL_PARAM		"illegal param"
-#define EXIST				"resource exist"
-#define UNEXIST				"resource unexist"
-#define NULL_PTR			"null point"
-#define NOT_CONFIG			"not configure attribute before used"
-#define NOT_SUPPORT			"operation or type is not supported by NOW"
-#define NOT_PERM			"operation is not permitted"
-#define NOT_NOMEM			"failure caused by malloc memory"
-#define NOT_NOBUF			"failure caused by malloc buffer"
-#define BUF_EMPTY			"no data in buffer"
-#define BUF_FULLE			"no buffer for new data"
-#define SYS_NOTREADY		"System is not ready,maybe not initialed or loaded"
-#define BADADDR				" bad address"
-#define BUSY				"resource is busy"
-#define ERR_MODULE_ID		"invalid device id"
-#define ERR_PARSE_JSON		"parse json failed"
-#define JSON_OBJ_EMPTY		"json data is empty"
-#define SET_EMPTY			"json set is empty"
-#define MATCH				"match failure"
-#define INITED				"Already initialized"
-#define INVALID_MUXTYPE		"invalid rec muxer type"
-#define FRAME_RATE		"frame rate anomaly"
+#define ERR_INVALID_DEVID_STR			"invalid device id"
+#define ERR_INVALID_CHNID_STR			"invalid channel id"
+#define ERR_ILLEGAL_PARAM_STR			"illegal param"
+#define ERR_EXIST_STR					"resource exist"
+#define ERR_UNEXIST_STR					"resource unexist"
+#define ERR_NULL_PTR_STR				"null point"
+#define ERR_NOT_CONFIG_STR				"not configure attribute before used"
+#define ERR_NOT_SUPPORT_STR				"operation or type is not supported by NOW"
+#define ERR_NOT_PERM_STR				"operation is not permitted"
+#define ERR_NOT_NOMEM_STR				"failure caused by malloc memory"
+#define ERR_NOT_NOBUF_STR				"failure caused by malloc buffer"
+#define ERR_BUF_EMPTY_STR				"no data in buffer"
+#define ERR_BUF_FULLE_STR				"no buffer for new data"
+#define ERR_SYS_NOTREADY_STR			"System is not ready,maybe not initialed or loaded"
+#define ERR_BADADDR_STR					" bad address"
+#define ERR_BUSY_STR					"resource is busy"
+#define ERR_MODULE_ID_STR				"invalid module id"
+#define ERR_PARSE_JSON_STR				"parse json failed"
+#define ERR_JSON_OBJ_EMPTY_STR			"json data is empty"
+#define ERR_JSON_SET_EMPTY_STR				"json set is empty"
+#define ERR_MATCH_STR					"match failure"
+#define ERR_INITED_STR					"Already initialized"
+#define ERR_INVALID_MUXTYPE_STR			"invalid rec muxer type"
+#define ERR_FRAME_RATE_STR				"frame rate anomaly"
 
-typedef int (*get_err_info_cb_t)(int err_value, char *out_buf, int buf_size);
-int32_t gavin_err_get_err_reason(int32_t err_value, char *out_buf, int32_t buf_size);
+typedef int32_t (*get_custom_err_info_cb_t)(int32_t err_value, int8_t *out_buf, int32_t buf_size);
+int32_t gavin_err_get_err_reason(int32_t err_value, int8_t *out_buf, int32_t buf_size);
 int32_t gavin_err_unregister_output_func(int32_t module_id);
-int32_t gavin_err_register_output_func(int32_t module_id, const char*module_name, get_err_info_cb_t get_err_info_cb);
+int32_t gavin_err_register_output_func(int32_t module_id, const int8_t *module_name, get_custom_err_info_cb_t get_err_info_cb);
 
 //**********一套给外部管理错误ID跟错误详细描述的接口start***************/
-int32_t gavin_err_info_list_add_node(struct list_head *list_head, int32_t err_id, const char *err_info);
+int32_t gavin_err_info_list_add_node(struct list_head *list_head, int32_t err_id, const int8_t *err_info);
 int32_t gavin_err_info_list_del_node(struct list_head *list_head, int32_t err_id);
-int32_t gavin_err_info_list_find_err_info(struct list_head *list_head, int32_t err_id, char *out_buf, int32_t buf_size);
+int32_t gavin_err_info_list_find_err_info(struct list_head *list_head, int32_t err_id, int8_t *out_buf, int32_t buf_size);
 int32_t gavin_err_info_list_init(struct list_head *list_head);
 int32_t gavin_err_info_list_uninit(struct list_head *list_head);
 
